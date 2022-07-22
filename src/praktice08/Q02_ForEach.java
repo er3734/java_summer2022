e172f391a85a655f4e1cd695765def0e18ea7fd8
Erdi Balta <erdi3734balta@gmail.com>
2022-07-22 21:08:27 +0300

    Initial commit

---
+++ src/praktice08/Q02_ForEach.java
@@ -0,0 +1,12 @@
+package praktice08;
+
+public class Q02_ForEach {
+
+    // String[] list = {"fruits", "vegetables", "meat", "milk"};
+    // print array with for each loop
+    // print length of each element
+
+    // Part 2:
+    // Print all the elements from array using for eachLoop
+    // If an element starts with 'v' then quit the loop
+}
